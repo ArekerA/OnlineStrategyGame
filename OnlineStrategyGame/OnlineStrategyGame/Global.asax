31ffad12be3f205e350f94ebba4f9e0d17dfd3c0
ArekerA <Arek.ludwikowski@hotmail.com>
2020-02-12 21:49:49 +0100

    Add basic project

---
+++ OnlineStrategyGame/OnlineStrategyGame/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineStrategyGame.MvcApplication" Language="C#" %>
---
+++ OnlineStrategyGame/OnlineStrategyGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineStrategyGame.Startup))]
+namespace OnlineStrategyGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
