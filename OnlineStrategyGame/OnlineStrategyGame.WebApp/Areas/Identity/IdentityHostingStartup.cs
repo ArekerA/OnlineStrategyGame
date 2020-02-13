b0dff2e962ebb3a304b2b81a470f0daec6b77580
ArekerA <Arek.ludwikowski@hotmail.com>
2020-02-13 21:41:36 +0100

    Setup projects

---
+++ OnlineStrategyGame/OnlineStrategyGame.WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineStrategyGame.Database.MSSQL.Data;
+
+[assembly: HostingStartup(typeof(OnlineStrategyGame.WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace OnlineStrategyGame.WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
