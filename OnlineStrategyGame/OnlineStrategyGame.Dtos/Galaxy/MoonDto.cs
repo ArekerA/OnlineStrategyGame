25edae3e5df819766087e5e373e247dac7aefdf2
ArekerA <Arek.ludwikowski@hotmail.com>
2020-02-26 20:19:08 +0100

    Add dto for moon and resources

---
+++ OnlineStrategyGame/OnlineStrategyGame.Dtos/Galaxy/MoonDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStrategyGame.Dtos.Galaxy
+{
+    public class MoonDto
+    {
+        public int Id { get; set; }
+        public ResourcesDto Resources { get; set; }
+    }
+}
---
+++ OnlineStrategyGame/OnlineStrategyGame.Dtos/Galaxy/ResourcesDto.cs
@@ -0,0 +1,24 @@
+﻿namespace OnlineStrategyGame.Dtos.Galaxy
+{
+    public class ResourcesDto
+    {
+        public int Id { get; set; }
+
+        public PlanetDto Planet { get; set; }
+        public int? PlanetId { get; set; }
+        public MoonDto Moon { get; set; }
+        public int? MoonId { get; set; }
+
+        public long Carbon { get; set; }
+        public long Uranium { get; set; }
+        public long Titanium { get; set; }
+        public long Aluminium { get; set; }
+        public long Hydrogen { get; set; }
+        public long Food { get; set; }
+        public long Graphene { get; set; }
+        public long AluminiumAlloy { get; set; }
+        public long TitaniumAlloy { get; set; }
+        public long Antimatter { get; set; }
+        public long Helium3 { get; set; }
+    }
+}
